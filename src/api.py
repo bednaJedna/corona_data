7b1a439b6c601808cb5981d9e86af416fcdfe0ac
dzcxs6c <radek.bednarik@tesena.com>
2020-03-29 12:06:03 +0200

    branch init

--- src/api.py
+++ src/api.py
@@ -1,16 +1,25 @@
 import csv
 import json
-from typing import Any, Text
+from datetime import datetime as dt
+from pprint import PrettyPrinter
+from typing import Any, Dict, Text
 
 import pandas as p
 import requests as r
 
-API_BASE = "https://coronavirus-19-api.herokuapp.com/"
+from src.utils.private import NEWS_API_KEY
+
+HEROKU_API_BASE = "https://coronavirus-19-api.herokuapp.com/"
+NEWS_API_BASE = "https://newsapi.org/v2/everything"
 MAP_DATA = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
 
+def _get_today() -> str:
+    return dt.now().strftime("%Y-%m-%d")
+
+
 def get_all_countries_data() -> Any:
-    reurl: Text = f"{API_BASE}countries"
+    reurl: Text = f"{HEROKU_API_BASE}countries"
     result: Any = r.get(reurl)
 
     return p.read_json(json.dumps(result.json(), ensure_ascii=False))
@@ -36,3 +45,31 @@ def get_map_data() -> Any:
     data = data.append(g_china, ignore_index=True)
 
     return p.concat([data.iloc[:, 0], data.iloc[:, 2:]], axis=1)
+
+
+def get_news(
+    q="COVID",
+    from_=_get_today,
+    sortBy="publishedAt",
+    apiKey=NEWS_API_KEY,
+    pageSize="10",
+    page="1",
+    language="en",
+) -> Any:
+    payload: Dict[str, str] = {
+        "q": q,
+        "from": from_,
+        "sortBy": sortBy,
+        "apiKey": apiKey,
+        "pageSize": pageSize,
+        "page": page,
+        "language": language,
+    }
+    response: Any = r.get(NEWS_API_BASE, params=payload)
+
+    return response.json()
+
+
+if __name__ == "__main__":
+    p: Any = PrettyPrinter(indent=2)
+    p.pprint(get_news())
